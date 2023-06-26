2282d57af2cb207266a4ba85849a76bdc3a05d06
Anton Serdyukov <serdyukov.anton@gmail.com>
2023-06-26 11:05:58 +0300

    outliers_quantile

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -61,7 +61,7 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
 
 # change for GitHub
 
-def quantile(data, feature, left=0.01, right=0.99):
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
     x = data[feature]
     lower_bound = x.quantile(left)
     upper_bound = x.quantile(right)
